a96cb98ee103c2df50c64c6e9aaa47590298eb70
Rebecca Jiang <rebeccajiang@wireless-10-147-70-21.public.utexas.edu>
2016-09-28 15:41:39 -0500

    DFSHelpers -> Helpers (actually changed)

--- Main.java
+++ Main.java
@@ -19,122 +19,128 @@ import java.io.*;
 
 
 public class Main {
-	
-	// static variables and constants only here.
-	static DFSHelpers helpers;
-	static int depth;
-	static ArrayList<String> visited;
-	static String startWord, endWord;
-	
-	public static void main(String[] args) throws Exception {
-		
-		Scanner kb;	// input Scanner for commands
-		PrintStream ps;	// output file
-		// If arguments are specified, read/write from/to files instead of Std IO.
-		if (args.length != 0) {
-			kb = new Scanner(new File(args[0]));
-			ps = new PrintStream(new File(args[1]));
-			System.setOut(ps);			// redirect output to ps
-		} else {
-			kb = new Scanner(System.in);// default from Stdin
-			ps = System.out;			// default to Stdout
-		}
-		initialize();
-	}
-	
-	public static void initialize() {
-		// initialize your static variables or constants here.
-		// We will call this method before running our JUNIT tests.  So call it 
-		// only once at the start of main.
-		helpers = new DFSHelpers();
-		depth = 0;
-		visited = new ArrayList<String>();
-	}
-	
-	/**
-	 * @param keyboard Scanner connected to System.in
-	 * @return ArrayList of 2 Strings containing start word and end word. 
-	 * If command is /quit, return empty ArrayList. 
-	 */
-	public static ArrayList<String> parse(Scanner keyboard) {
-		String input = keyboard.nextLine();
-		if (input.equals("/quit")) {
-			System.exit(0);
-		}
-		
-		int i = 0;
-		while (input.charAt(i) != ' ') {
-			i++;
-		}
-		startWord = input.substring(0,  i);
-		startWord = startWord.toUpperCase();
-		endWord = input.substring(i + 1, input.length());
-		endWord = endWord.toUpperCase();
-		
-		ArrayList<String> words = new ArrayList<String>();
-		words.add(startWord);
-		words.add(endWord);
-		return words;
-	}
-	/**
-	 * This method creates a word ladder using DFS
-	 * @param start is the start word
-	 * @param end is the end word
-	 * @return the word ladder
-	 */
-	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		start = start.toUpperCase();
-		end = end.toUpperCase();
-		Set<String> dict = makeDictionary();
-		ArrayList<String> neighbors = helpers.getNeighbors(start, dict, visited);
-		
-		// Base Case: Found a word that is directly connected to end
-		if (neighbors.contains(end)) {
-			ArrayList<String> finPath = new ArrayList<String>();
-			finPath.add(end);
-			finPath.add(start);
-			return finPath;
-		}
-		
-		// No path
-		if (neighbors.isEmpty()) {
-			return null;
-		}
-		
-		// Allow this path to be used again
-		visited.add(start);
-		
-		neighbors = helpers.sortNeighbors(neighbors, end);
-		ArrayList<String> path = new ArrayList<String>();
-		for (String currentWord : neighbors) {
-			depth++;
-			path = getWordLadderDFS(currentWord, end);
-			depth--;
-			
-			if (path == null) { // Dead end, don't use this path again
-				visited.add(currentWord);
-				continue;
-			}
-			else { // Found a working path
-				break;
-			}
-		}
-		
-		// No path found
-		if (path == null) {
-			return null;
-		}
-		
-		path.add(start);
-		if (depth == 0) { // Finished ladder
-			startWord = start;
-			endWord = path.get(0);
-			visited.clear();
-			path = helpers.reverse(path);
-		}
-		return path;
-	}
-	
+    
+    // static variables and constants only here.
+    static Helpers helper;
+    static int depth;
+    static ArrayList<String> visited;
+    static String startWord, endWord;
+    
+    public static void main(String[] args) throws Exception {
+        
+        Scanner kb;	// input Scanner for commands
+        PrintStream ps;	// output file
+        // If arguments are specified, read/write from/to files instead of Std IO.
+        if (args.length != 0) {
+            kb = new Scanner(new File(args[0]));
+            ps = new PrintStream(new File(args[1]));
+            System.setOut(ps);			// redirect output to ps
+        } else {
+            kb = new Scanner(System.in);// default from Stdin
+            ps = System.out;			// default to Stdout
+        }
+        initialize();
+        ArrayList<String> input = parse(kb);
+        /*		 ArrayList<String> ladder = getWordLadderDFS(input.get(0), input.get(1));
+         ArrayList<String> ladder2 = getWordLadderBFS(input.get(0), input.get(1));
+         printLadder(ladder);
+         System.out.println("\nbfs: ");
+         printLadder(ladder2);*/
+    }
+    
+    public static void initialize() {
+        // initialize your static variables or constants here.
+        // We will call this method before running our JUNIT tests.  So call it
+        // only once at the start of main.
+        helper = new Helpers();
+        depth = 0;
+        visited = new ArrayList<String>();
+    }
+    
+    /**
+     * @param keyboard Scanner connected to System.in
+     * @return ArrayList of 2 Strings containing start word and end word.
+     * If command is /quit, return empty ArrayList.
+     */
+    public static ArrayList<String> parse(Scanner keyboard) {
+        String input = keyboard.nextLine();
+        if (input.equals("/quit")) {
+            System.exit(0);
+        }
+        
+        int i = 0;
+        while (input.charAt(i) != ' ') {
+            i++;
+        }
+        startWord = input.substring(0,  i);
+        startWord = startWord.toUpperCase();
+        endWord = input.substring(i + 1, input.length());
+        endWord = endWord.toUpperCase();
+        
+        ArrayList<String> words = new ArrayList<String>();
+        words.add(startWord);
+        words.add(endWord);
+        return words;
+    }
+    /**
+     * This method creates a word ladder using DFS
+     * @param start is the start word
+     * @param end is the end word
+     * @return the word ladder
+     */
+    public static ArrayList<String> getWordLadderDFS(String start, String end) {
+        start = start.toUpperCase();
+        end = end.toUpperCase();
+        Set<String> dict = makeDictionary();
+        ArrayList<String> neighbors = helper.getNeighbors(start, dict, visited);
+        
+        // Base Case: Found a word that is directly connected to end
+        if (neighbors.contains(end)) {
+            ArrayList<String> finPath = new ArrayList<String>();
+            finPath.add(end);
+            finPath.add(start);
+            return finPath;
+        }
+        
+        // No path
+        if (neighbors.isEmpty()) {
+            return null;
+        }
+        
+        // Allow this path to be used again
+        visited.add(start);
+        
+        neighbors = helper.sortNeighbors(neighbors, end);
+        ArrayList<String> path = new ArrayList<String>();
+        for (String currentWord : neighbors) {
+            depth++;
+            path = getWordLadderDFS(currentWord, end);
+            depth--;
+            
+            if (path == null) { // Dead end, don't use this path again
+                visited.add(currentWord);
+                continue;
+            }
+            else { // Found a working path
+                break;
+            }
+        }
+        
+        // No path found
+        if (path == null) {
+            return null;
+        }
+        
+        path.add(start);
+        if (depth == 0) { // Finished ladder
+            startWord = start;
+            endWord = path.get(0);
+            visited.clear();
+            path = helper.reverse(path);
+        }
+        return path;
+    }
+    
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
         
         Set<String> dict = makeDictionary();
@@ -179,34 +185,34 @@ public class Main {
         return null;
     }
     
-	public static Set<String>  makeDictionary () {
-		Set<String> words = new HashSet<String>();
-		Scanner infile = null;
-		try {
-			infile = new Scanner (new File("five_letter_words.txt"));
-		} catch (FileNotFoundException e) {
-			System.out.println("Dictionary File not Found!");
-			e.printStackTrace();
-			System.exit(1);
-		}
-		while (infile.hasNext()) {
-			words.add(infile.next().toUpperCase());
-		}
-		return words;
-	}
-	
-	public static void printLadder(ArrayList<String> ladder) {
-		// No ladder exists
-		if (ladder == null) {
-			System.out.println("no word ladder can be found between " + startWord.toLowerCase() + " and " + endWord.toLowerCase() + ".");
-			return;
-		}
-		
-		// Ladder does exist
-		System.out.println("a " + (ladder.size() - 2) + "-rung word ladder exists between " + startWord.toLowerCase() + " and " + endWord.toLowerCase() + ".");
-		for (int i = 0; i < ladder.size(); i++) {
-			System.out.println(ladder.get(i).toLowerCase());
-		}
-		return;
-	}
+    public static Set<String>  makeDictionary () {
+        Set<String> words = new HashSet<String>();
+        Scanner infile = null;
+        try {
+            infile = new Scanner (new File("five_letter_words.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Dictionary File not Found!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (infile.hasNext()) {
+            words.add(infile.next().toUpperCase());
+        }
+        return words;
+    }
+    
+    public static void printLadder(ArrayList<String> ladder) {
+        // No ladder exists
+        if (ladder == null) {
+            System.out.println("no word ladder can be found between " + startWord.toLowerCase() + " and " + endWord.toLowerCase() + ".");
+            return;
+        }
+        
+        // Ladder does exist
+        System.out.println("a " + (ladder.size() - 2) + "-rung word ladder exists between " + startWord.toLowerCase() + " and " + endWord.toLowerCase() + ".");
+        for (int i = 0; i < ladder.size(); i++) {
+            System.out.println(ladder.get(i).toLowerCase());
+        }
+        return;
+    }
 }
